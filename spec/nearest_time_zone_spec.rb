11fc7b070aa838b7753284051a4d7b95a8ef29d1
Tanner Swenson <tanner@jetinsight.com>
2017-02-21 16:03:51 -0800

    add tests and redo dump

--- spec/nearest_time_zone_spec.rb
+++ spec/nearest_time_zone_spec.rb
@@ -1,13 +1,21 @@
 describe NearestTimeZone do
-  it "should have the method to" do
+  it 'has the method to' do
     expect { NearestTimeZone.to(1, 3) }.not_to raise_error
   end
 
-  it "should find the time zone name nearest to coordinates" do
-    expect(NearestTimeZone.to(41.8500, -87.6500)).to eq "America/Chicago"
+  it 'finds the time zone name nearest to coordinates' do
+    expect(NearestTimeZone.to(41.8500, -87.6500)).to eq 'America/Chicago'
   end
 
-  it "should return Pacific/Honolulu timezone for Palmyra Atoll island" do
-    expect(NearestTimeZone.to(5.883, -162.06)).to eq "Pacific/Honolulu"
+  it 'returns Pacific/Honolulu timezone for Palmyra Atoll island' do
+    expect(NearestTimeZone.to(5.883, -162.06)).to eq 'Pacific/Honolulu'
+  end
+
+  it 'returns Pacific/Midway timezone for Midway island' do
+    expect(NearestTimeZone.to(28.2, -177.4)).to eq 'Pacific/Midway'
+  end
+
+  it 'returns America/Santo_Domingo timezone for Turks and Caicos Islands island' do
+    expect(NearestTimeZone.to(21.77, -72.26)).to eq 'America/Santo_Domingo'
   end
 end
